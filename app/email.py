c5b137be55ed63acb9143e17f338dcf0787a52cb
Fridah-Alwanga <fridahalwanga6@gmail.com>
2020-12-16 17:11:04 +0300

    add

--- app/email.py
+++ app/email.py
@@ -1,7 +1,7 @@
 from flask_mail import Message
 from flask import render_template
 from . import mail
-subject_pref = 'New Blog'
+subject_pref = 'Blog'
 
 def mail_message(subject, template, to, **kwargs):
     sender_email = 'fridahalwanga6@gmail.com'
